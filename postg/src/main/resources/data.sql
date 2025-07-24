39ea80b46f414e71da97b6a1ccc866da5bf7e5ef
Sulaiman <SulaimanHasan411@gmail.com>
2025-07-23 23:03:23 -0400

    make own postgres db schema and add data

---
+++ postg/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'Widget A', 'Testing purposes'),
+(2, 'Widget B', 'Entertainment'),
+(3, 'Widget C', 'Productivity'),
+(4, 'Widget D', 'Outdoors'),
+(5, 'Widget E', 'Well-being');
